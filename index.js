791de0115bc916fd8662e8b6c51c475d007a4f0f
Adriano Zambrana Marchetti <adrianozambrana@protonmail.com>
2021-01-19 11:06:38 -0400

    Minimized time spent on each terminal line

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-import delay from 'https://cdn.skypack.dev/delay';
-import * as scpDb from './scpDb.json';
+const delay = (d)=>{
+    return new Promise((res,rej)=>{
+        setTimeout(res,d);
+    });
+}
 
 const terminal = document.getElementById("terminal");
 const lineLimit = 100;
 const addLine = (newLine)=>{
-    const lineArr = terminal.innerText.split("\n");
+    let lineArr = terminal.innerText.split("\n");
     lineArr.push(newLine);
     lineArr = lineArr.slice(0,100);
     terminal.innerText = lineArr.join("\n");
@@ -22,14 +25,16 @@ const messageTemplates = [
     function(scp){
         return `Accident related to SCP-${scp[0]} ongoing. Be advised.`
     }
-]
+];
 
-(async()=>{
+(async ()=>{
+    const scpDbResponse = await fetch("./scpDb.json");
+    const scpDb = await scpDbResponse.json();
     while(true){
-        const secondsToWait = Math.random()*10 + 2;
+        const secondsToWait = Math.random()*60 + 10;
         await delay(secondsToWait*1000);
         const randomSCP = scpDb[Math.floor(Math.random()* scpDb.length)];
-        const randomTemplate = messageTemplates[Math.floor(Math.random()*scpDb.length)];
+        const randomTemplate = messageTemplates[Math.floor(Math.random()*messageTemplates.length)];
         addLine(randomTemplate(randomSCP));
     }
 })();
